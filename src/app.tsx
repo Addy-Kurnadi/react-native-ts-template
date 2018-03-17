e44b3a7b378f4454c7868c77081fde53101943b9
Adiwena Kurnadi <adiwena23@gmail.com>
2018-03-17 14:49:24 +1100

    project init

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { Component } from "react"
+import * as React from "react"
+import { Platform, StyleSheet, Text, View } from "react-native"
+
+export class App extends Component<any, any> {
+	constructor(props: any) {
+		super(props)
+	}
+
+	render() {
+		const test = "tes"
+
+		return (
+			<View>
+				<Text>{"asdas"}</Text>
+			</View>
+		)
+	}
+}
